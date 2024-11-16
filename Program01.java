e0f7001f4bc237b1d68308db9ac7809ebcb6279f
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-11-16 08:39:38 +0530

    ok

--- Program01.java
+++ Program01.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Program01 {
+    //WAP to find the addition of two numbers
     public static void main(String[] args) {
-        System.out.println("Please Enter the following detials:");
+        Scanner sc=new Scanner(System.in);
+System.out.println("Enter the first number:");
+   int a=sc.nextInt();
+   
+System.out.println("Enter the second number:");
+   int b=sc.nextInt();
+sc.close();
+   System.out.println("The addition of two number is: "+(a+b));
     }
 }
