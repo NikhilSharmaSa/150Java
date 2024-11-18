63888ce22fd108229ee10e20f560d066cde50c4c
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-11-18 17:00:28 +0530

    Program30

---
+++ Program30.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Program30 {
+
+    public static int find_factorial(int n){
+        if(n==0|| n==1){
+            return 1;
+        }
+
+        return n*find_factorial(n-1);
+    }
+    public static void main(String[] args) {
+        //WAP to find the nCr
+Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n:");
+        int n=sc.nextInt();
+        System.out.println("Enter the value of r:");
+        int r=sc.nextInt();
+        sc.close();
+        int ans=find_factorial(n)/(find_factorial(r)*find_factorial(n-r));
+        System.out.println(ans);
+    }
+}
