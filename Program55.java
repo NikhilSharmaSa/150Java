fe9692eee7615501ff865a25614160f8110b75ca
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-11-20 21:27:11 +0530

    Program55

---
+++ Program55.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Program55 {
+    public static void main(String[] args) {
+         //WAP to print the following pattern 
+              /*
+                        
+                        EEEEE
+                        DDDDD
+                        CCCCC
+                        BBBBB
+                        AAAAA
+              */
+              Scanner sc=new Scanner(System.in);
+              System.out.println("Enter the Number of rows:");
+              int n=sc.nextInt();
+              char ch='E';
+              sc.close();
+              for(int i=1; i<=n;i++){
+              
+                for(int j=1; j<=n; j++){
+                    System.out.print(ch);
+                
+                }
+                System.out.println();
+                ch--;
+              }
+    }
+    
+}
