c55e1172a9946320dd0ce92337cba1504ef86bdc
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-11-25 22:30:31 +0530

    Program77

---
+++ Program77.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Program77 {
+    public static void main(String[] args) {
+        /*
+          *
+          ***
+          *****
+          *******
+          *********
+         */
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter the number of row:");
+        int n=sc.nextInt();
+        sc.close();
+       
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=2*i-1; j++){
+System.out.print('*');
+
+            }
+            System.out.println();
+        }
+
+
+    }
+}
