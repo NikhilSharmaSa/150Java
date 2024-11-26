0aae4cd2a3edf57dd2668caf034620dc2b68838d
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-11-26 20:47:22 +0530

    Program86&87

---
+++ Program86.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Program86 {
+    public static void main(String[] args) {
+          //WAP to print the following pattern 
+              /*
+                           1
+                          123
+                         12345
+                        1234567
+                       123456789
+              */
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Number of rows:");
+        int n=sc.nextInt();
+        sc.close();
+
+        for(int i=1; i<=n; i++){
+            for(int k=1; k<=n-i; k++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=2*i-1; j++){
+                
+                System.out.print(j);
+            }
+           
+            System.out.println();
+        }
+    }
+}
