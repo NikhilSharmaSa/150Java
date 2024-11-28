64c28482c1961437afae0f2e688e41c915757fa8
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-11-28 07:46:05 +0530

    Change

--- Program72.java
+++ Program72.java
@@ -13,12 +13,10 @@ public class Program72 {
         System.out.println("Please enter the number of row:");
         int n=sc.nextInt();
         sc.close();
-       
         for(int i=n; i>=1; i--){
             for(int k=0; k<n-i;k++){
                 System.out.print(" ");
             }
-           
             for(int j=1; j<=i; j++){
 System.out.print(j);
             }
