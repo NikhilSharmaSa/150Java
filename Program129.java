182b53762f36e9bc234a1ee946b1a23405539804
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-01 12:43:03 +0530

    Program129

--- Program129.java
+++ Program129.java
@@ -1,6 +1,6 @@
 
 public class Program129 {
-    public static void increasingArr(int[] arr){
+    public static void decresingArr(int[] arr){
 for(int i=0; i<arr.length-1; i++){
     for(int j=i+1; j<arr.length; j++){  
  if(arr[i]<arr[j]){
@@ -16,7 +16,7 @@ for(int i=0; i<arr.length-1; i++){
           //WAP to sort an array in decreasing order
                      int[] arr={-12,2,88,4,5,4,5,6,4,5,44,1,5,8};
           
-         increasingArr(arr);
+         decresingArr(arr);
          for (int i : arr) {
             System.out.print(i+" ");
          }
