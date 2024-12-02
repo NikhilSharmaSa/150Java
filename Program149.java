c43551d2acd15fdcfebad75b6b49a25be55f0ccc
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-02 21:36:55 +0530

    Program149

---
+++ Program149.java
@@ -0,0 +1,19 @@
+
+public class Program149 {
+    public static void printReverse(int[] arr){
+      
+for(int i=arr.length-1; i>=0; i--){
+    System.out.print(arr[i]+" ");
+  
+}
+}
+    
+    public static void main(String[] args) {
+          //WAP to print an array in reverse order
+                     int[] arr={-12,2,88,4,5,4,5,6,4,5,44,1,5,8}; 
+        printReverse(arr);
+        
+
+
+    }
+}
