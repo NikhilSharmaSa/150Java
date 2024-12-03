6a3ab0bae221fc19e397957de60ec2df797441a4
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-03 21:29:38 +0530

    Program134

--- Program134.java
+++ Program134.java
@@ -14,6 +14,7 @@ int[] arr= {14,16,12,18,15,14,12,13};
 System.out.print("Please enter the Value:");
 Scanner sc=new Scanner(System.in);
 int value=sc.nextInt();
+
 System.out.print("Enter the Index :");
 int index=sc.nextInt();
 sc.close();
