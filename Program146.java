24ac6bd224a847d626c148bc6087f2094194d3b6
nikhil.pandit.901@gmail.com <nikhil.pandit.901@gmail.com>
2024-12-10 23:26:44 +0530

    Program146.java

---
+++ Program146.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+public class Program146 {
+    public static void printFibonacci(int key){
+     
+}
+
+
+    public static void main(String[] args) {
+          //WAP to print the Fibonacci Series
+                     System.out.print("Please Enter The Value of n:"); 
+                     Scanner sc=new Scanner(System.in);
+                     int key=sc.nextInt();
+                     printFibonacci(key);
+                     sc.close();
+        
+        
+
+    }
+}
